b649777e51eec09df4f5fe49ddc2769e744e2d6d
simonju <simonju@tencent.com>
2024-04-08 13:10:09 +0800

    a

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig((env) => {
   const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
 
   return {
-    base: '/chat/',
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(process.cwd(), 'src'),
@@ -37,7 +37,7 @@ export default defineConfig((env) => {
       port: 1002,
       open: false,
       proxy: {
-        '/chat/api': {
+        '/api': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
           rewrite: path => path.replace('/api/', '/'),
